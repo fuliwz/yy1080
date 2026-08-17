a605a1c4f76f18c78cf61e10cf10939426847c81
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-17 15:55:34 +0800

    Clone application shell

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Header />
+  <AdContainer />
+  <router-view />
+  <Footer />
+</template>
+
+<script setup>
+import { watch } from 'vue'
+import { useRoute } from 'vue-router'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import AdContainer from './components/AdContainer.vue'
+
+const route = useRoute()
+
+watch(
+  () => route.fullPath,
+  () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+    if (!document.title || document.title === '18xx') {
+      document.title = '18xx'
+    }
+  },
+  { immediate: true }
+)
+</script>
