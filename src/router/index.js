73b1e27e885167e52898c099815fed5e42742600
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-17 15:55:53 +0800

    Clone Vue router

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Play from '../views/Play.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/play/:id', name: 'play', component: Play, props: true },
+  { path: '/category/:id/:page?', name: 'category', component: Category, props: true },
+  { path: '/search', name: 'search', component: Search }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior() { return { top: 0, left: 0 } }
+})
