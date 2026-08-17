d170f9f5386e31539e8c2fce0dfabf3ffc1db819
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-17 21:03:39 +0800

    Update [[path]].js

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   const url = new URL(context.request.url)
 
   const target =
-    'https://slapibf.com/' +
+    'https://api.heiapi.cc/' +
     url.pathname.replace('/api/', '') +
     url.search
 
