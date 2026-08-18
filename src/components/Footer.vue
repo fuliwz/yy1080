2cc96d5711e27ad1e00582875b7c3a5e324c13e2
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-18 14:20:01 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,39 +5,6 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  // 防止重复加载
-  if (window.__histats_loaded) return
-
-  window.__histats_loaded = true
-
-  window._Hasync = window._Hasync || []
-
-  window._Hasync.push([
-    'Histats.start',
-    '1,4657865,4,0,0,0,00010000'
-  ])
-
-  window._Hasync.push([
-    'Histats.fasi',
-    '1'
-  ])
-
-  window._Hasync.push([
-    'Histats.track_hits',
-    ''
-  ])
-
-  const hs = document.createElement('script')
-
-  hs.type = 'text/javascript'
-  hs.async = true
-  hs.src = '//s10.histats.com/js15_as.js'
-
-  ;(document.head || document.body).appendChild(hs)
-})
 </script>
 
 <style scoped>
