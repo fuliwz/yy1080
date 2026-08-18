ad09cb45344630a87d902af85eaea2667ae30a41
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-18 14:16:00 +0800

    Update tj.js

--- public/tj.js
+++ public/tj.js
@@ -1,9 +1,82 @@
-var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4657865,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+(function () {
+  'use strict';
+
+  var lastUrl = '';
+
+  window._Hasync = window._Hasync || [];
+
+  window._Hasync.push([
+    'Histats.start',
+    '1,4657865,4,0,0,0,00010000'
+  ]);
+
+  window._Hasync.push([
+    'Histats.fasi',
+    '1'
+  ]);
+
+  window._Hasync.push([
+    'Histats.track_hits',
+    ''
+  ]);
+
+  function trackPage() {
+    var url = window.location.href;
+
+    if (url === lastUrl) {
+      return;
+    }
+
+    lastUrl = url;
+
+    window._Hasync.push([
+      'Histats.track_hits',
+      ''
+    ]);
+  }
+
+  // 第一次访问
+  trackPage();
+
+  // Vue Router 使用 history.pushState
+  var originalPushState = history.pushState;
+
+  history.pushState = function () {
+    var result = originalPushState.apply(
+      history,
+      arguments
+    );
+
+    setTimeout(trackPage, 100);
+
+    return result;
+  };
+
+  // 浏览器前进 / 后退
+  window.addEventListener(
+    'popstate',
+    function () {
+      setTimeout(trackPage, 100);
+    }
+  );
+
+  // 加载 Histats
+  if (
+    !document.querySelector(
+      'script[data-histats="1"]'
+    )
+  ) {
+    var hs = document.createElement('script');
+
+    hs.type = 'text/javascript';
+    hs.async = true;
+    hs.dataset.histats = '1';
+    hs.src =
+      'https://s10.histats.com/js15_as.js';
+
+    (
+      document.head ||
+      document.body
+    ).appendChild(hs);
+  }
 })();
