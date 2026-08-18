7dffc0bff695c48d7c93bf9566e602a96f2a8af8
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-18 14:19:03 +0800

    Update tj.js

--- public/tj.js
+++ public/tj.js
@@ -1,8 +1,4 @@
 (function () {
-  'use strict';
-
-  var lastUrl = '';
-
   window._Hasync = window._Hasync || [];
 
   window._Hasync.push([
@@ -20,63 +16,19 @@
     ''
   ]);
 
-  function trackPage() {
-    var url = window.location.href;
-
-    if (url === lastUrl) {
-      return;
-    }
-
-    lastUrl = url;
-
-    window._Hasync.push([
-      'Histats.track_hits',
-      ''
-    ]);
+  if (document.querySelector('script[data-histats="1"]')) {
+    return;
   }
 
-  // 第一次访问
-  trackPage();
+  var hs = document.createElement('script');
 
-  // Vue Router 使用 history.pushState
-  var originalPushState = history.pushState;
+  hs.type = 'text/javascript';
+  hs.async = true;
+  hs.dataset.histats = '1';
+  hs.src = 'https://s10.histats.com/js15_as.js';
 
-  history.pushState = function () {
-    var result = originalPushState.apply(
-      history,
-      arguments
-    );
-
-    setTimeout(trackPage, 100);
-
-    return result;
-  };
-
-  // 浏览器前进 / 后退
-  window.addEventListener(
-    'popstate',
-    function () {
-      setTimeout(trackPage, 100);
-    }
-  );
-
-  // 加载 Histats
-  if (
-    !document.querySelector(
-      'script[data-histats="1"]'
-    )
-  ) {
-    var hs = document.createElement('script');
-
-    hs.type = 'text/javascript';
-    hs.async = true;
-    hs.dataset.histats = '1';
-    hs.src =
-      'https://s10.histats.com/js15_as.js';
-
-    (
-      document.head ||
-      document.body
-    ).appendChild(hs);
-  }
+  (
+    document.head ||
+    document.body
+  ).appendChild(hs);
 })();
