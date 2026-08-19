7ca3928e6f572cd5e7886cebe72e15d9c64b1b77
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-19 23:33:28 +0800

    feat: add history route

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Home from '../views/Home.vue'
 import Play from '../views/Play.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
+import History from '../views/History.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/play/:id', name: 'play', component: Play, props: true },
   { path: '/category/:id/:page?', name: 'category', component: Category, props: true },
-  { path: '/search', name: 'search', component: Search }
+  { path: '/search', name: 'search', component: Search },
+  { path: '/history', name: 'history', component: History }
 ]
 
 export default createRouter({
