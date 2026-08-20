179bb0eb84f58c1092148d4388a54deddb6278ea
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-20 21:41:11 +0800

    Update links.js

--- public/links.js
+++ public/links.js
@@ -2,43 +2,67 @@
   "91xxx.cyou": [
     {
       "name": "绿色导航",
-      "url": "https://淼弢.greendh3.net/屾覇h"
+      "url": "https://韝i犇.greendh.org/驫翧y"
     },
     {
-      "name": "找AV导航",
-      "url": "https://esaeu.zavdh93.vip/sead9h"
+      "name": "柠檬导航",
+      "url": "https://邬焺.nmdh18.com/p嶴q"
     },
     {
-      "name": "水多多导航",
-      "url": "https://www.sddtz15.cc"
+      "name": "花小猪导航",
+      "url": "https://燚r.9qqq.cc/飽翧"
     },
     {
-      "name": "XFuLi导航",
-      "url": "https://1xdaohang.xyz"
+      "name": "妖涩梨导航",
+      "url": "https://嗷嗷嗷嗷.yaosl1.sbs/?mark=Q1TN3T"
+    },
+    {
+      "name": "娱乐青导航",
+      "url": "http://819ylq.yuleqing17.cc/app/Mf4viP"
+    },
+    {
+      "name": "红浪漫导航",
+      "url": "http://819llm.hlmhs3.top/app/gcbfcV"
+    },
+    {
+      "name": "双马尾导航",
+      "url": "http://819smw.smaw3.top/app/TtgjWr"
+    },
+    {
+      "name": "东方色导航",
+      "url": "http://819dong.dfswh3.top/app/Af2z8v"
     },
     {
       "name": "冈本导航",
-      "url": "http://614ben.gangben3.top/app/7Vk05h"
+      "url": "http://614ben.gangben3.top/app/0BU7ve"
     },
     {
-      "name": "找AV导航",
-      "url": "https://esaeu.zavdh93.vip/sead9h"
-    },
-    {
-      "name": "水多多导航",
-      "url": "https://www.sddtz15.cc"
-    },
-    {
-      "name": "XFuLi导航",
-      "url": "https://1xdaohang.xyz"
-    },
-    {
-      "name": "冈本导航",
-      "url": "http://614ben.gangben3.top/app/7Vk05h"
+      "name": "荔枝导航",
+      "url": "http://627lizhii.lizhidh3.top/app/tosXlA"
     }
   ],
 
   "ys91h.top": [
+    {
+      "name": "蓝色导航",
+      "url": "https://j7th.bluedaohang.cc/bzxeoq3"
+    },
+    {
+      "name": "蓝导航",
+      "url": "https://6m543t.landh17.party/3xpss6"
+    },
+    {
+      "name": "花小猪导航",
+      "url": "https://籩f蓧.9qqq.cc/n59"
+    },
+    {
+      "name": "6DD导航",
+      "url": "http://6146d.5ddd6.top/app/v9NtgY"
+    },
+    {
+      "name": "红颜导航",
+      "url": "http://614hy.hongyan2.top/app/Ais9PO"
+    },
     {
       "name": "蓝色导航",
       "url": "https://j7th.bluedaohang.cc/bzxeoq3"
